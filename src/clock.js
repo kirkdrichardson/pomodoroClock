425c06b07c0b59f5ca342492ea8336192794a941
Kirk <kirk.douglas.richardson@gmail.com>
2017-08-31 19:33:01 -0600

    clock working, bottom button functionality needed

--- src/clock.js
+++ src/clock.js
@@ -36,7 +36,6 @@ const TopButtonBar = (props) => {
 }
 
 const BottomButtonBar = (props) => {
-    let now = new Date().toLocaleTimeString();
     return (
       <div>
         <Button name={"Start"} click={props.start} />
@@ -47,8 +46,25 @@ const BottomButtonBar = (props) => {
 }
 
 
+const ChangeTimeToggle = (props) => {
+  const style = {
+    fontSize: 36,
+    fontWeight: "bold",
+    display: "inline-block"
+  };
+  return (
+    <button style={style} onClick={() => props.click()}>
+      {props.symbol}
+    </button>
+  );
+}
+
+
 const Timer = (props) => {
-    const timerStyle = {fontSize: 72}
+    const timerStyle = {
+      fontSize: 72,
+      display: "inline-block"
+    }
     return (
       <div>
         <h2 style={timerStyle}>{props.remainingTime}</h2>
@@ -61,62 +77,140 @@ class Clock extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      remainingTime: "25:00"
+      remainingTime: "25:00",
+      clockRunning: false
     }
   }
-  addMinutes = (date, minutes) => {
+
+/*
+Helper functions
+*/
+
+  // takes date & time to add to given date as arguments
+  getStoppingTime = (date, minutes) => {
     return new Date(date.getTime() + minutes*60000);
 }
-  getRemainingTime = (stoppingTime) => {
-    // refactor so that state is updated every second
-    let remaining;
-    setInterval(function() {
+
+// returns an obj w/ minute & second keys with 'number' values
+currentTimeObj = (stateTime) => {
+  // handles cases where state is a message str & not a time str
+  if (stateTime.length !== 5) {
+    this.setState({ remainingTime: "25:00"});
+  }
+
+  // capture minutes & seconds from current state string
+  const matchArr = stateTime.match(/^(\d+):(\d+)$/)
+  const lengthInMinutes = parseInt(matchArr[1]);
+  const lengthInSeconds = parseInt(matchArr[2]);
+
+  return {
+    minute: lengthInMinutes,
+    second: lengthInSeconds
+  }
+}
+
+// returns Boolean if time is up
+isTimeLeft = (stateTime) => {
+  const timeObj = this.currentTimeObj(stateTime);
+  return (timeObj.minute > 0 || timeObj.second > 0) ?
+     true :
+     false;
+   }
+
+  //updates state w/ remaining time
+  setRemainingTime = (stoppingTime, interval) => {
+    // stops clock if time is up
+    if (this.isTimeLeft(this.state.remainingTime) === false) {
+      clearInterval(interval);
+      this.setState({
+        remainingTime: "Time's up!",
+        clockRunning: false
+      });
+      return;
+    }
+
       let now = new Date().getTime(),
           diff = stoppingTime - now,
-
+            // calculate remaining min/sec from milliseconds
           minutesLeft = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60)),
           secondsLeft = Math.floor((diff % (1000 *60)) / 1000),
-
+            // format time
           minutes = minutesLeft < 10 ? "0" + minutesLeft : minutesLeft,
           seconds = secondsLeft < 10 ? "0" + secondsLeft : secondsLeft,
 
           remaining = minutes + ":" + seconds;
-
-    }, 1000);
+      this.setState({
+        remainingTime: remaining
+      });
 }
+
+// starts clock
+tick = (stopTime, setRemainingTimeCallback) => {
+  let interval = setInterval(function(){
+    setRemainingTimeCallback(stopTime, interval);}, 990);
+}
+
+
+/*
+Bottom buttons
+*/
+
+startClock = () => {
+  // cancel function call if clock already running
+  if (this.state.clockRunning)
+    return;
+
+  if (this.isTimeLeft(this.state.remainingTime)) {
+    const matchArr = this.state.remainingTime.match(/^(\d+):(\d+)$/)
+    const lengthInMinutes = parseInt(matchArr[1]);
+    const stopAt = this.getStoppingTime(new Date(), lengthInMinutes);
+    this.tick(stopAt, this.setRemainingTime);
+    this.setState({ clockRunning: true });
+  }
+  else
+    alert("Whoops! No time was set.")
+
+}
+
+
+pauseClock = () => {
+this.setState({
+  remainingTime: "paused"
+});
+}
+
+resetClock = () => {
+this.setState({
+  remainingTime: "reset"
+});
+}
+
+/*
+add or subtract minute methods
+*/
+
+addMinute = () => {
+  const timeObj = this.currentTimeObj(this.state.remainingTime);
+  const newTimeStr = String(timeObj.minute + 1) + ':' + String(timeObj.second);
+  this.setState({ remainingTime: newTimeStr });
+}
+
+subtractMinute = () => {
+  alert("connected");
+}
+
+
+/* Top button bar buttons, only insert new time */
   pomodoroClock = () => {
-    const stopAt = this.addMinutes(new Date(), 25).getTime(); // create time to stop at in milliseconds
-    const temp = this.getRemainingTime(stopAt);
-    this.setState({
-      remainingTime: temp
-    })
+    this.setState({ remainingTime: "25:00" });
   }
   shortBreakClock = () => {
-    this.setState({
-      remainingTime: "5:00"
-    });
+    this.setState({ remainingTime: "5:00" });
   }
   longBreakClock = () => {
-    this.setState({
-      remainingTime: "10:00"
-    });
-  }
-  startClock = () => {
-    this.setState({
-      remainingTime: "started"
-    });
+    this.setState({ remainingTime: "10:00" });
   }
 
-  pauseClock = () => {
-    this.setState({
-      remainingTime: "paused"
-    });
-  }
-  resetClock = () => {
-    this.setState({
-      remainingTime: "reset"
-    });
-  }
 
   render() {
     return (
@@ -129,7 +223,11 @@ class Clock extends Component {
           shortBreak={this.shortBreakClock}
           longBreak={this.longBreakClock} />
 
-        <Timer remainingTime={this.state.remainingTime}/>
+          <div style={{display: "flex", justifyContent: "center"}}>
+            <ChangeTimeToggle symbol={String.fromCharCode(43)} click={this.addMinute}/>
+            <Timer  remainingTime={this.state.remainingTime}/>
+            <ChangeTimeToggle symbol={String.fromCharCode(8722)} click={this.subtractMinute}/>
+          </div>
 
         <BottomButtonBar
           start={this.startClock}
